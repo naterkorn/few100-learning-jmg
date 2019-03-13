beda0a1301304ab797019ac0bddc9e99912d1bc2
Nate Niederkorn <niederkorn.4@gmail.com>
2019-03-13 19:56:27 +0000

    End of second day.

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,12 @@
+describe('Writing a spec', () => {
+    it('is sick nasty my man, good on you.', () => {
+        //given
+        const a = 10, b = 20;
+
+        //when
+        const answer = a + b;
+
+        //then
+        expect(answer).toBe(30);
+    });
+});
--- spec/utils.ts
+++ spec/utils.ts
@@ -1,1 +1,9 @@
-export const add = (a:number, b:number) => a + b;
+type Transformer = (x: string) => string;
+export function formatName(
+    first: string,
+    last: string,
+    transformer: Transformer = (x) => x): string {
+    return transformer(`${last}, ${first}`);
+}
+
+
